52627d078aa0920b23b805b028fbb7ef002bab5d
rchekashov <romanrich89@gmail.com>
2023-08-30 13:32:31 +0200

    http3

--- webtransport/main.go
+++ webtransport/main.go
@@ -13,11 +13,11 @@ const keyFile =  "/etc/letsencrypt/live/look.ovh/privkey.pem"
 func main() {
 	// create a new webtransport.Server, listening on (UDP) port 443
 	s := webtransport.Server{
-		H3: http3.Server{Addr: ":4433"},
+		H3: http3.Server{Addr: ":443"},
 	}
 
 	// Create a new HTTP endpoint /webtransport.
-	http.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/webtransport", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := s.Upgrade(w, r)
 		if err != nil {
 			log.Printf("upgrading failed: %s", err)
