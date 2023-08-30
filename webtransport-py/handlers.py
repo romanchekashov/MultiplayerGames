9f3bfb451b39212f4b75ba16d41434cdf90da799
rchekashov <romanrich89@gmail.com>
2023-08-31 00:16:55 +0200

    remote

--- webtransport-py/handlers.py
+++ webtransport-py/handlers.py
@@ -33,7 +33,7 @@ class CounterHandler:
     
     def send_datagram(self, data) -> None:
         payload = str(data).encode(self._encoding)
-        self._http.send_datagram(self._session_id, payload[2:-1])
+        self._http.send_datagram(self._session_id, payload[2:-1]) # remove b'<str_bytes>'
 
     def h3_event_received(self, event: H3Event) -> None:
         if isinstance(event, DatagramReceived):
