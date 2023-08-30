946b699ad6173a8b85aaf405fb82aa7ff113bf9f
rchekashov <romanrich89@gmail.com>
2023-08-30 15:18:50 +0200

    http3

---
+++ webtransport-py/run.sh
@@ -0,0 +1,2 @@
+echo "Starting webtransport_server"
+python3 webtransport_server.py /etc/letsencrypt/live/look.ovh/fullchain.pem /etc/letsencrypt/live/look.ovh/privkey.pem
--- webtransport-py/webtransport_server.py
+++ webtransport-py/webtransport_server.py
@@ -197,8 +197,6 @@ class WebTransportProtocol(QuicConnectionProtocol):
         self._http.send_headers(
             stream_id=stream_id, headers=headers, end_stream=end_stream)
 
-DEFAULT_certFile =  "/etc/letsencrypt/live/look.ovh/fullchain.pem"
-DEFAULT_keyFile =  "/etc/letsencrypt/live/look.ovh/privkey.pem"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -211,7 +209,7 @@ if __name__ == '__main__':
         is_client=False,
         max_datagram_frame_size=65536,
     )
-    configuration.load_cert_chain(args.certificate or DEFAULT_certFile, args.key or DEFAULT_keyFile)
+    configuration.load_cert_chain(args.certificate, args.key)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(
