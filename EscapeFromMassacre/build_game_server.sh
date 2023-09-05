6cb9362d29c9abf9ce3a3934878e577f4bbf55c2
rchekashov <romanrich89@gmail.com>
2023-09-05 09:56:22 +0200

    fix

--- EscapeFromMassacre/build_game_server.sh
+++ EscapeFromMassacre/build_game_server.sh
@@ -19,8 +19,9 @@ rm -r build_server/build
 echo "Running bob.jar"
 echo "For help run: java -jar bob.jar -h"
 echo "use resolve to download dependencies"
-java -jar bob.jar --variant=headless clean resolve build --platform $PLATFORM --variant headless --settings game_server.settings
-# java -jar bob.jar --variant=headless clean build --platform $PLATFORM --variant headless --settings game_server.settings
+# java -jar bob.jar clean resolve build --platform $PLATFORM --variant release --settings game_server.settings
+# java -jar bob.jar clean build --platform $PLATFORM --variant headless --settings game_server.settings
+java -jar bob.jar clean build --platform $PLATFORM --variant release --settings game_server.settings
 
 cp -r ./build ./build_server/build
 
