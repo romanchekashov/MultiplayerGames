a4403b70ed1d30a94ca8a44679c6dc9debe4860e
rchekashov <romanrich89@gmail.com>
2023-09-05 16:24:55 +0200

    fix asyncio.exceptions.IncompleteReadError: 0 bytes read on a total of 4 expected bytes

--- webtransport-py/server_game_connector.py
+++ webtransport-py/server_game_connector.py
@@ -13,20 +13,25 @@ async def handle_client(reader, writer):
     set_game_server_communication(reader, writer)
     request = None
     while request != 'quit':
-        # request = (await reader.read(255)).decode('utf8')
-        in_len = await reader.readexactly(4)
-        size = struct.unpack('>L', in_len)[0]
-        in_bytes = await reader.readexactly(size)
-        in_msg = in_bytes.decode('utf8')
+        try:
+            # request = (await reader.read(255)).decode('utf8')
+            in_len = await reader.readexactly(4)
+            size = struct.unpack('>L', in_len)[0]
+            in_bytes = await reader.readexactly(size)
+            in_msg = in_bytes.decode('utf8')
 
-        # print(f'in_msg {in_msg}')
-        # out_data = stream_encode(in_msg)
-        # writer.write(out_data)
-        # print(f'in_msg {in_msg}, out {out_data}')
+            # print(f'in_msg {in_msg}')
+            # out_data = stream_encode(in_msg)
+            # writer.write(out_data)
+            # print(f'in_msg {in_msg}, out {out_data}')
 
-        await to_game_client(in_msg)
+            await to_game_client(in_msg)
+            await writer.drain()
+        except Exception as e:
+            # asyncio.exceptions.IncompleteReadError: 0 bytes read on a total of 4 expected bytes
+            error_message = "resetting connection: {}".format(e.args)
+            Log.error(error_message)
 
-        await writer.drain()
     writer.close()
 
 async def run_game_server_connector(host, port):
