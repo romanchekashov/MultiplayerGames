76d68dde69b6b0d794cafb1171581ee817422391
rchekashov <romanrich89@gmail.com>
2023-09-06 18:36:49 +0200

    docker deploy

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 echo "Kill existing service instance"
 kill -9 $(lsof -t -i:4433)
 
-echo "Starting PROD server..."
+echo "Starting server..."
 cd webtransport-py
-python3 main.py /etc/letsencrypt/live/look.ovh/fullchain.pem /etc/letsencrypt/live/look.ovh/privkey.pem
+python3.10 main.py certificate/fullchain.pem certificate/privkey.pem
--- run_dev.sh
+++
@@ -1,7 +0,0 @@
-echo "Kill existing service instance"
-kill -9 $(lsof -t -i:4433)
-
-echo "Starting DEV servers..."
-# python3.10 main.py /Users/romanchekashov/workspace/example.com+5.pem /Users/romanchekashov/workspace/example.com+5-key.pem
-cd webtransport-py
-python3.10 main.py certificate/fullchain.pem certificate/privkey.pem
