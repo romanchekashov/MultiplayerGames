1eed941fbcbddcdfe9c07db63dcdab4d14c6f8fb
rchekashov <romanrich89@gmail.com>
2023-09-06 16:30:11 +0200

    fix

--- webtransport-py/server_game_connector.py
+++ webtransport-py/server_game_connector.py
@@ -13,26 +13,21 @@ async def handle_client(reader, writer):
     set_game_server_communication(reader, writer)
     request = None
     while request != 'quit':
-        try:
             # request = (await reader.read(255)).decode('utf8')
             in_len = await reader.readexactly(4)
             size = struct.unpack('>L', in_len)[0]
             in_bytes = await reader.readexactly(size)
             in_msg = in_bytes.decode('utf8')
 
-            # print(f'in_msg {in_msg}')
-            # out_data = stream_encode(in_msg)
-            # writer.write(out_data)
-            # print(f'in_msg {in_msg}, out {out_data}')
-
             await to_game_client(in_msg)
             await writer.drain()
-        except Exception as e:
-            # asyncio.exceptions.IncompleteReadError: 0 bytes read on a total of 4 expected bytes
-            error_message = "resetting connection: {}".format(e.args)
-            Log.error(error_message)
-            request = 'quit'
-            writer.write_eof()
+        # try:
+        # except Exception as e:
+        #     # asyncio.exceptions.IncompleteReadError: 0 bytes read on a total of 4 expected bytes
+        #     error_message = "resetting connection: {}".format(e.args)
+        #     Log.error(error_message)
+        #     request = 'quit'
+            # writer.write_eof()
             # raise Exception(error_message)
 
     writer.close()
