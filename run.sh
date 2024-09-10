a5494574a918d69e43017a0780539b3fce3d891a
rchekashov <romanrich89@gmail.com>
2024-09-10 22:21:46 +0300

    read certificate from env file

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 echo "Kill existing service instance"
 kill -9 $(lsof -t -i:4433)
 
-#echo "Starting server..."
-#cd webtransport-py
+# Use passed arguments
+CERT=$1
+CERT_KEY=$2
+
+echo "Starting server..."
+cd webtransport-py
+python3.10 main.py $CERT $CERT_KEY
 #python3.10 main.py certificate/fullchain.pem certificate/privkey.pem
